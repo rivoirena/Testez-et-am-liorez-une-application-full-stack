717e4719f2b44e87177e25d97fecd2de73f61033
rivoirena <rivoiren38@gmail.com>
2026-09-20 12:31:28 +0200

    refactor: move business logic from controllers to services, remove direct repository access

--- back/src/main/java/com/openclassrooms/starterjwt/exception/GlobalExceptionHandler.java
+++ back/src/main/java/com/openclassrooms/starterjwt/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.starterjwt.exception;
 
+import com.openclassrooms.starterjwt.payload.response.MessageResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,5 +11,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NumberFormatException.class)
     public ResponseEntity<Void> handleNumberFormatException(NumberFormatException ex) {
         return ResponseEntity.badRequest().build();
+    }
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<MessageResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(new MessageResponse(ex.getMessage()));
     }
 }
--- back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
+++ back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
@@ -1,15 +1,22 @@
 package com.openclassrooms.starterjwt.services;
 
+import com.openclassrooms.starterjwt.exception.EmailAlreadyExistsException;
 import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.payload.request.SignupRequest;
 import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void delete(Long id) {
@@ -18,5 +25,24 @@ public class UserService {
 
     public User findById(Long id) {
         return this.userRepository.findById(id).orElse(null);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User createUser(SignupRequest signUpRequest) {
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new EmailAlreadyExistsException("Error: Email is already taken!");
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getEmail(),
+                signUpRequest.getLastName(),
+                signUpRequest.getFirstName(),
+                passwordEncoder.encode(signUpRequest.getPassword()),
+                false);
+        return userRepository.save(user);
     }
 }
